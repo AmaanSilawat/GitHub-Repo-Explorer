cbaa4d86da649e66b2e5c8838fd99bc31a73b6c1
amaansilawat16-lang <amaansilawat16@gmail.com>
2026-06-03 23:32:34 +0530

    add github api integration with caching

--- server/routes/githubRoutes.js
+++ server/routes/githubRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const axios = require("axios");
+const cache = require("../cache/cache");
+const { fetchGithubData } = require("../services/githubService");
 
 const router = express.Router();
 
@@ -7,21 +8,26 @@ router.get("/:username", async (req, res) => {
   try {
     const { username } = req.params;
 
-    const userResponse = await axios.get(
-      `https://api.github.com/users/${username}`
-    );
+    const cachedData = cache.get(username);
 
-    const repoResponse = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
+    if (cachedData) {
+      return res.json({
+        source: "cache",
+        ...cachedData,
+      });
+    }
+
+    const data = await fetchGithubData(username);
+
+    cache.set(username, data);
 
     res.json({
-      user: userResponse.data,
-      repos: repoResponse.data,
+      source: "github",
+      ...data,
     });
   } catch (error) {
     res.status(404).json({
-      message: "User not found",
+      message: "GitHub user not found",
     });
   }
 });
